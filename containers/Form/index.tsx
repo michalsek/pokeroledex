71eddc365de649f5f1ae02fcb4e103c9ed8af7c6
Michał Sęk <michal.sek@swmansion.com>
2022-01-16 00:48:22 +0100

    working

--- containers/Form/index.tsx
+++ containers/Form/index.tsx
@@ -1,7 +1,9 @@
 import HPInput from './HPInput';
+import WillInput from './WillInput';
 
 const Form = {
   HPInput,
+  WillInput,
 };
 
 export default Form;
--- screens/TrainerScreen/index.tsx
+++ screens/TrainerScreen/index.tsx
@@ -38,65 +38,12 @@ const TrainerScreen: React.FC<RootTabScreenProps<'Trainer'>> = (props) => {
         </View>
         <View style={{ flex: 3 }}>
           <Form.HPInput characterPath="trainer" editable={editMode} />
+          <Layout.Stack size="small" />
+          <Form.WillInput characterPath="trainer" editable={editMode} />
         </View>
       </View>
     </Layout.Screen>
   );
-  // return (
-
-  //     <View
-  //       style={{
-  //         borderWidth: 4,
-  //         borderRadius: 25,
-  //         flexDirection: 'row',
-  //         borderColor: '#414042',
-  //         backgroundColor: '#fff',
-  //       }}
-  //     >
-  //       <View style={{ flex: 1 }}>
-  //         <Text
-  //           style={{
-  //             fontWeight: '700',
-  //             textTransform: 'uppercase',
-  //             paddingLeft: 13,
-  //             fontSize: 12,
-  //           }}
-  //         >
-  //           Name
-  //         </Text>
-  //         <Form.TextInput
-  //           valuePath="trainer.name"
-  //           style={{
-  //             fontSize: 20,
-  //             paddingHorizontal: 10,
-  //             paddingBottom: 5,
-  //           }}
-  //         />
-  //       </View>
-
-  //       <View style={{ flex: 1 }}>
-  //         <Text
-  //           style={{
-  //             fontWeight: '700',
-  //             textTransform: 'uppercase',
-  //             fontSize: 12,
-  //             paddingLeft: 10,
-  //           }}
-  //         >
-  //           Age
-  //         </Text>
-  //         <Form.NumberInput
-  //           valuePath="trainer.age"
-  //           style={{
-  //             fontSize: 20,
-  //             paddingHorizontal: 10,
-  //             paddingBottom: 5,
-  //           }}
-  //         />
-  //       </View>
-  //     </View>
-  //   </Layout.Screen>
-  // );
 };
 
 export default TrainerScreen;
